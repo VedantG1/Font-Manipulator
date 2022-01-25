9e5d50173341a8a3a7a820583293bb775239a3f8
Vedant <82694223+VedantG1@users.noreply.github.com>
2022-01-25 18:21:05 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+var user, noseX, noseY, size, leftWristX, rightWristX
 function setup(){
     canvas = createCanvas(550, 500)
     canvas.position(550, 150)
@@ -7,8 +8,17 @@ function setup(){
     poseNet = ml5.poseNet(video, modelLoaded)
     poseNet.on('pose', gotPoses)
 }
+
+function submit(){
+    user = document.getElementById("name").value
+    document.getElementById("name").value = ""
+}
 function draw(){
     background("grey")
+    stroke("#F90093")
+    fill("#F90093")
+    textSize(size)
+    text(user, noseX, noseY)
 }
 function modelLoaded(){
     console.log("PoseNet is Initialized")
@@ -16,11 +26,13 @@ function modelLoaded(){
 function gotPoses(results){
     if(results.length > 0){
         console.log(results)
+        noseX = results[0].pose.nose.x
+        noseY = results[0].pose.nose.y
+        leftWristX = results[0].pose.leftWrist.x
+        rightWristX = results[0].pose.rightWrist.x
+        size = floor(leftWristX - rightWristX)
     }
     else{
         console.log("Error")
     }
-}
-function submit(){
-    var name = document.getElementById("name").value
 }
